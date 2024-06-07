7fc7ca417e4ed78beb44ee90b80215d521f931f0
haithamassoli <haitham.b.assoli@gmail.com>
2024-06-07 23:56:39 +0300

    update packages

--- babel.config.js
+++ babel.config.js
@@ -2,7 +2,6 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ["babel-preset-expo"],
-    plugins: ["react-native-reanimated/plugin", "expo-router/babel"],
     env: {
       production: {
         plugins: ["transform-remove-console", "react-native-paper/babel"],
